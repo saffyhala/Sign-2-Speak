daf29401872d6213e7e0c2c0dc9350dec0b059c2
t94363ha <hala.alsaffarini@student.manchester.ac.uk>
2024-04-13 13:15:05 +0100

    camera inversion problem fixed

--- dataset/capture_images.py
+++ dataset/capture_images.py
@@ -1,11 +1,9 @@
 import os
 import cv2
 
-# This script captures images from the webcam and saves them in the data folder.
 DATA_DIR = './data'
 if not os.path.exists(DATA_DIR):
     os.makedirs(DATA_DIR)
-
 
 number_of_classes = 36
 dataset_size = 100
@@ -20,6 +18,7 @@ for j in range(number_of_classes):
     done = False
     while True:
         ret, frame = cap.read()
+        frame = cv2.flip(frame, 1)  # Flip the frame horizontally
         cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
                     cv2.LINE_AA)
         cv2.imshow('frame', frame)
@@ -29,6 +28,7 @@ for j in range(number_of_classes):
     counter = 0
     while counter < dataset_size:
         ret, frame = cap.read()
+        frame = cv2.flip(frame, 1)  # Flip the frame horizontally
         cv2.imshow('frame', frame)
         cv2.waitKey(25)
         cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
